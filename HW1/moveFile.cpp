19afe99888dd2fff9ee122dbbae572740b3a6a5e
Logan Passi <loganpassi88@gmail.com>
2020-02-05 21:15:00 -0600

    Completed HW1

---
+++ HW1/moveFile.cpp
@@ -0,0 +1,45 @@
+/*
+Logan Passi
+01/30/20
+copyFileExample.cpp
+*/
+
+#include <stdio.h>
+#include <Windows.h> //<--Windows API
+#include <fileapi.h>
+
+using namespace std;
+
+int main(int arcg, char * args[]) {
+
+	HANDLE inFile;
+	HANDLE outFile;
+	DWORD numberOfBytesRead;
+	DWORD numberOfBytesWritten;
+
+	char buffer[20];
+
+	inFile = CreateFileA(args[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL); //System Call
+
+	if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+		printf("Could not find file!");
+		return 1;
+	}
+
+	outFile = CreateFileA(args[2], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	while (true) {
+		ReadFile(inFile, buffer, 20, &numberOfBytesRead, NULL);
+		if (numberOfBytesRead == 0) {
+			break;
+		}
+
+		WriteFile(outFile, buffer, numberOfBytesRead, &numberOfBytesWritten, NULL);
+	}
+
+	CloseHandle(inFile);
+	CloseHandle(outFile);
+
+	DeleteFileA(args[1]);
+	return 0;
+}
